ddf8f41af510fc741c87ad038813000ddb6ccbbd
adouch9 <mr.ouldmoussa@gmail.com>
2021-04-07 16:38:44 +0200

    jeux

---
+++ intro.js
@@ -0,0 +1,9 @@
+const title = `                    Tentez de deviner le mot secret en entrant des lettres une par une au clavier. 
+                      Ne gaspillez pas vos coups, car si trop de vos choix sont erronés vous tuerez 
+                                     le pendu et vous perdrez la partie.                                        
+                                     
+              `
+
+
+
+exports.title = title
--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 
    //-----------------------------------Import API d'outils de travail (Package.json)-------------------------------------------
 
-   const fs             = require('fs')                                  
+const fs             = require('fs')                                  
 const { randomInt }     = require('crypto')
 const readlineSync      = require('readline-sync')
 const chalk             = require('chalk');
 const { hangman }       = require('./hangman.js')
+const { title }       = require('./intro.js')
+const { header }       = require('./header.js')
 
    //-----------------------------------Déclaration des variables assigner au jeux----------------------------------------------
 
@@ -15,32 +17,37 @@ const transformStringInArray = dictionnaire.split('\n')                // transf
 const n = randomInt(0, transformStringInArray.length)                 // variable assigner pour utiliser le random pour l'array du dictionnaire
 const motSecret = Array(transformStringInArray[n].length).fill('_')   // Tableaux pour récuperer les lettres du mot secret remplacer par les _
 let count = 7
-//console.log(chalk.greenBright(`BullMarket Prédiction: ${chalk.yellow(transformStringInArray[n])} pour la fin de l'anner 2021`))        // test d'afichage du programme
-console.log(chalk.greenBright(`Tentez de deviner le mot secret en entrant des lettres une par une au clavier. Ne gaspillez pas vos coups, car si trop de vos choix sont erronés vous tuerez le pendu et vous perdrez la partie.`))
-   
+console.log(header)
+console.log(chalk.greenBright(`BullMarket Prédiction: ${chalk.yellow(transformStringInArray[n])} pour la fin de l'anner 2021`))        // test d'afichage du programme
+ console.log(chalk.greenBright(title))
+  
 //------------------------------------Programme du jeux-----------------------------------------------------------------------
 
-while (motSecret.includes('_')) {
-  console.log(motSecret.join(" "))
-  let question = readlineSync.question(chalk.yellow(`Qu'elle est votre lettre?`))
 
-  if(transformStringInArray[n].includes(question)){
+
+while (motSecret.includes('_')) {
+  // console.log(motSecret.join(" "))
+  let question = readlineSync.question(chalk.yellow(`Qu'elle est votre lettre: ${motSecret.join(" ")} ? `)).toUpperCase() 
+  
+if(transformStringInArray[n].includes(question)){
     for (let i = 0; i < motSecret.length; i++) {
-  if(transformStringInArray[n][i] === question){
-   motSecret[i] = question
+       if(transformStringInArray[n][i] === question){
+          motSecret[i] = question
+   
      }
    }
-  }
-  if(!transformStringInArray[n].includes(question)){
+  } else {
     count --
     console.log(chalk.redBright(`Cette lettre ${question} est incorrect il vous reste ${count} chances ${hangman[count]}`))
-  }
-  if (!transformStringInArray[n].includes(question) && count <= 0){
+ }
+
+if (!transformStringInArray[n].includes(question) && count <= 0){
     console.log(chalk.blueBright(`Vous avez perdu ! Le mot secret était  ${chalk.yellow(transformStringInArray[n])}`))
     process.exit(1)
+ }else if(motSecret.join("") === transformStringInArray[n]){
+   console.log(`Vous avais Gagner Mot secret: ${chalk.grey(transformStringInArray[n])}`)
+ }
 
-  }
-  if(motSecret === transformStringInArray[n]){
-    console.log('bien jouer')
-  }
 }
+
+
